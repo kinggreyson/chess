c8a196889650ea3a1cf7a8ba906a81e7dde7cc1f
kinggreyson <kingg14@byu.edu>
2026-05-20 12:15:01 -0600

    DataAccess Interface Implemented

---
+++ server/src/main/java/dataaccess/DataAccess.java
@@ -0,0 +1,27 @@
+package dataaccess;
+
+import model.AuthData;
+import model.GameData;
+import model.UserData;
+import org.eclipse.jetty.server.Authentication;
+
+import java.util.List;
+
+public interface DataAccess {
+    void clear() throws DataAccessException;
+
+    //UserData
+    void createUser(UserData user) throws DataAccessException;
+    UserData getUser(String username) throws DataAccessException;
+
+    //GameData
+    void createGame(GameData game) throws DataAccessException;
+    GameData getGame(int gameID) throws DataAccessException;
+    List<GameData> listGames() throws DataAccessException;
+    void updateGame(GameData game) throws DataAccessException;
+
+    //AuthData
+    void createAuth(AuthData auth) throws DataAccessException;
+    AuthData getAuth(String authToken) throws DataAccessException;
+    void deleteAuth(String authToken) throws DataAccessException;
+}
