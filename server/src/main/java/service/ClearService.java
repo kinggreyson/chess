66e743e1602d2a9958706bca6dc3dfb4cedf2cb7
kinggreyson <kingg14@byu.edu>
2026-05-20 16:32:42 -0600

    ClearService implemented

--- server/src/main/java/service/ClearService.java
+++ server/src/main/java/service/ClearService.java
@@ -1,4 +1,19 @@
 package service;
 
+import dataaccess.DataAccess;
+import dataaccess.DataAccessException;
+
 public class ClearService {
+    private final DataAccess clearData;
+
+    public ClearService(DataAccess dataAccess)
+    {
+        this.clearData = dataAccess;
+    }
+
+    public void clear() throws DataAccessException
+    {
+        clearData.clear();
+    }
+
 }
