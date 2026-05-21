48120581adf10090aca294d6ba642fbe87e2595e
kinggreyson <kingg14@byu.edu>
2026-05-20 23:40:28 -0600

    Code Quality Updates -Updated the singlestepmove pieces (king/knight) in the same way as the sliding pieces with a helper function in PieceMovesCalculator for their movement.

--- shared/src/main/java/chess/KnightMovesCalculator.java
+++ shared/src/main/java/chess/KnightMovesCalculator.java
@@ -6,28 +6,7 @@ import java.util.Collection;
 public class KnightMovesCalculator implements PieceMovesCalculator {
     @Override
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition position) {
-        Collection<ChessMove> possibleMoves = new ArrayList<>();
         int[][] directions = {{2, 1}, {1, 2}, {2, -1}, {1, -2}, {-1, 2}, {-2, 1}, {-1, -2}, {-2, -1}};
-
-        for (int[] direction : directions) {
-            int rowDir = direction[0];
-            int colDir = direction[1];
-            int currentRow = position.getRow() + rowDir;
-            int currentCol = position.getColumn() + colDir;
-
-            if (currentRow >= 1 && currentRow <= 8 && currentCol >= 1 && currentCol <= 8) {
-                ChessPosition newPosition = new ChessPosition(currentRow, currentCol);
-                ChessPiece target = board.getPiece(newPosition);
-
-                if (target != null) {
-                    if (target.getTeamColor() != board.getPiece(position).getTeamColor()) {
-                        possibleMoves.add(new ChessMove(position, newPosition, null));
-                    }
-                } else {
-                    possibleMoves.add(new ChessMove(position, newPosition, null));
-                }
-            }
-        }
-        return possibleMoves;
+        return PieceMovesCalculator.singleStepMoves(board, position, directions);
     }
 }
--- shared/src/main/java/chess/PieceMovesCalculator.java
+++ shared/src/main/java/chess/PieceMovesCalculator.java
@@ -31,5 +31,30 @@ public interface PieceMovesCalculator {
         }
         return possibleMoves;
     }
+
+    static Collection<ChessMove> singleStepMoves(ChessBoard board, ChessPosition position, int[][] directions) {
+        Collection<ChessMove> possibleMoves = new ArrayList<>();
+
+        for (int[] direction : directions) {
+            int rowDir = direction[0];
+            int colDir = direction[1];
+            int currentRow = position.getRow() + rowDir;
+            int currentCol = position.getColumn() + colDir;
+
+            if (currentRow >= 1 && currentRow <= 8 && currentCol >= 1 && currentCol <= 8) {
+                ChessPosition newPosition = new ChessPosition(currentRow, currentCol);
+                ChessPiece target = board.getPiece(newPosition);
+
+                if (target != null) {
+                    if (target.getTeamColor() != board.getPiece(position).getTeamColor()) {
+                        possibleMoves.add(new ChessMove(position, newPosition, null));
+                    }
+                } else {
+                    possibleMoves.add(new ChessMove(position, newPosition, null));
+                }
+            }
+        }
+        return possibleMoves;
+    }
 }
 
