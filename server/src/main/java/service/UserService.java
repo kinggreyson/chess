00900ab4733244d8d93db3c326b5485ce1320e11
kinggreyson <kingg14@byu.edu>
2026-05-20 19:07:45 -0600

    UserService implemented

--- server/src/main/java/service/UserService.java
+++ server/src/main/java/service/UserService.java
@@ -1,4 +1,86 @@
 package service;
+import dataaccess.DataAccess;
+import dataaccess.DataAccessException;
+import model.AuthData;
+import model.GameData;
+import model.UserData;
+import java.util.UUID;
 
-public class UserService {
+public class UserService
+{
+    private final DataAccess userData;
+
+    public UserService(DataAccess dataAccess)
+    {
+        this.userData = dataAccess;
+    }
+
+    public record RegisterRequest(String username, String password, String email)
+    {}
+    public record RegisterResult(String username, String authToken)
+    {}
+
+    public record LoginRequest(String username, String password)
+    {}
+    public record LoginResult(String username, String authToken)
+    {}
+
+    public RegisterResult register(RegisterRequest req) throws DataAccessException
+    {
+        // 400 Check - Empty username, password, or email
+        if (req.username == null || req.password == null || req.email == null)
+        {
+            throw new DataAccessException("Error: bad request");
+        }
+
+        //403 Check - Username already taken
+        if (userData.getUser(req.username) != null)
+        {
+            throw new DataAccessException(("Error: already taken"));
+        }
+
+        UserData user = new UserData(req.username(), req.password(), req.email());
+        userData.createUser(user);
+        String token = UUID.randomUUID().toString(); //Create randomized token
+        AuthData auth = new AuthData(token, req.username());
+        userData.createAuth(auth); //authToken
+
+        return new RegisterResult(req.username(), token);
+    }
+
+    public LoginResult login(UserService.LoginRequest req) throws DataAccessException
+    {
+        //400 Check - Empty Username or password
+        if (req.username == null || req.password == null)
+        {
+            throw new DataAccessException("Error: bad request");
+        }
+
+        UserData user = userData.getUser(req.username());
+
+        //401 Check - Incorrect username or password
+        if (user == null || !user.password().equals(req.password()))
+        {
+            throw new DataAccessException("Error: unauthorized");
+        }
+
+        String token = UUID.randomUUID().toString();
+        AuthData auth = new AuthData(token, req.username());
+        userData.createAuth(auth);
+
+        return new LoginResult(req.username(), token);
+    }
+
+    public void logout(String authToken) throws DataAccessException
+    {
+        //401 - AuthToken
+        if (userData.getAuth(authToken) == null)
+        {
+            throw new DataAccessException("Error: unauthorized");
+        }
+        //delete authToken
+        userData.deleteAuth(authToken);
+    }
 }
+
+
