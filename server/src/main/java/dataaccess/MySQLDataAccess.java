64edaa011370dfe09458818cfb702f7ce37a4684
kinggreyson <kingg14@byu.edu>
2026-05-26 22:08:48 -0600

    Table Statements + configureDatabase function

--- server/src/main/java/dataaccess/MySQLDataAccess.java
+++ server/src/main/java/dataaccess/MySQLDataAccess.java
@@ -22,6 +22,50 @@ public class MySQLDataAccess implements DataAccess {
         configureDatabase();
     }
 
-    //TODO Create tables
-    //TODO create configureDatabase() function
+    private final String[] createTables = {
+            """
+            CREATE TABLE IF NOT EXISTS users (
+                username VARCHAR(256) NOT NULL,
+                password VARCHAR(256) NOT NULL,
+                email VARCHAR(256) NOT NULL,
+                PRIMARY KEY (username)
+                )
+            """,
+            """
+            CREATE TABLE IF NOT EXISTS auth (
+                authtoken VARCHAR(256) NOT NULL,
+                username VARCHAR(256) NOT NULL,
+                PRIMARY KEY (authtoken)
+                )
+            """,
+            """
+            CREATE TABLE IF NOT EXISTS games (
+                gameID INT NOT NULL AUTO_INCREMENT,
+                whiteUsername VARCHAR(256),
+                blackUsername VARCHAR(256),
+                gameName VARCHAR(256) NOT NULL,
+                game TEXT NOT NULL,
+                PRIMARY KEY (gameID)
+                )
+            """
+    };
+
+    private void configureDatabase() throws DataAccessException
+    {
+        DatabaseManager.createDatabase();
+        try (var connection = DatabaseManager.getConnection())
+        {
+            for (var table : createTables)
+            {
+                try (var preparedTable = connection.prepareStatement(table))
+                {
+                    preparedTable.executeUpdate();
+                }
+                catch (Exception e)
+                {
+                    throw new DataAccessException("Unable to configure database: " + e.getMessage());
+                }
+            }
+        }
+    }
 }
