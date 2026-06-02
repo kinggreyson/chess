57a775e73de42316c164c1b3c2f11ee4cd8a59c0
kinggreyson <kingg14@byu.edu>
2026-06-01 23:07:59 -0600

    Helper function to run endpoints through

--- client/src/main/java/client/ServerFacade.java
+++ client/src/main/java/client/ServerFacade.java
@@ -16,12 +16,12 @@ import java.net.URL;
 
 public class ServerFacade {
 
-private final String Url;
+private final String serverUrl;
 private final Gson gson = new Gson();
 
 public ServerFacade(int port)
 {
-    this.Url = "http://localhost:" + port;
+    this.serverUrl = "http://localhost:" + port;
 }
 
 public AuthData register(String username, String password, String email)
@@ -52,5 +52,48 @@ public GameData[] listGames(String authToken)
 public void joinGame(String authToken, int gameID, String playerColor)
 {
 
+}
+
+private <T> T request(String method, String path, Object body, String authToken, Class<T> response) throws Exception
+{
+    URL url = (new URI(serverUrl + path)).toURL();
+    HttpURLConnection connection = (HttpURLConnection) + url.openConnection();
+    connection.setRequestMethod(method);
+    connection.setDoOutput(true);
+
+    if (authToken != null)
+    {
+        connection.setRequestMethod("authorization", authToken);
+    }
+
+    if(body != null)
+    {
+        connection.setRequestProperty("Content-Type", "application/json");
+        try (OutputStream out = connection.getOutputStream())
+        {
+            out.write(gson.toJson(body).getBytes());
+        }
+    }
+
+    connection.connect();
+
+    if(connection.getResponseCode() / 100 != 2)
+    {
+        try (InputStream error = connection.getErrorStream())
+        {
+            InputStreamReader read = new InputStreamReader(error);
+            var errorResponse = gson.fromJson(read, Map.class);
+            throw new Exception((String) errorResponse.get("message"));
+        }
+    }
+
+        if (response == null) {
+            return null;
+        }
+
+        try (InputStream is = connection.getInputStream();
+             InputStreamReader read = new InputStreamReader(is)) {
+            return gson.fromJson(read, response);
+        }
 }
 }
