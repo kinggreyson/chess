4cf37096e54e60da8850474e1deb32f48950afb8
kinggreyson <kingg14@byu.edu>
2026-06-01 20:47:18 -0600

    Initialized functions for each endpoint/connection

--- client/src/main/java/client/ServerFacade.java
+++ client/src/main/java/client/ServerFacade.java
@@ -2,6 +2,8 @@ package client;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
+import model.AuthData;
+import model.GameData;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -22,4 +24,33 @@ public ServerFacade(int port)
     this.Url = "http://localhost:" + port;
 }
 
+public AuthData register(String username, String password, String email)
+{
+
+}
+
+public AuthData login(String username, String password)
+{
+
+}
+
+public void logout(String authToken)
+{
+
+}
+
+public int createGame(String authToken, String gameName)
+{
+
+}
+
+public GameData[] listGames(String authToken)
+{
+
+}
+
+public void joinGame(String authToken, int gameID, String playerColor)
+{
+
+}
 }
