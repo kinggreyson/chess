c35ecb17ff8c42cbb8ad46aa65c871df104ba371
kinggreyson <kingg14@byu.edu>
2026-06-02 21:52:17 -0600

    ServerFacade game/clear functions set

--- client/src/main/java/client/ServerFacade.java
+++ client/src/main/java/client/ServerFacade.java
@@ -42,31 +42,42 @@ public void logout(String authToken) throws Exception
     request("DELETE", "/session", null, authToken, null); //Delete login, no body/response needed
 }
 
-public int createGame(String authToken, String gameName)
+public int createGame(String authToken, String gameName) throws Exception
 {
-
+    var makeup = Map.of("gameName", gameName);
+    var endpoint = request("POST", "/game", makeup, authToken, Map.class);
+    int gameID = (int)(double) endpoint.get("gameID"); //convert gameID to double from object for JSON, then convert to int
+    return (int) gameID;
 }
 
-public GameData[] listGames(String authToken)
+public GameData[] listGames(String authToken) throws Exception
 {
-
+    var endpoint = request("GET", "/game", null, authToken, Map.class);
+    var jsonGame = gson.toJson(endpoint.get("games")); //convert to json
+    return gson.fromJson(jsonGame, GameData[].class);
 }
 
-public void joinGame(String authToken, int gameID, String playerColor)
+public void joinGame(String authToken, int gameID, String playerColor) throws Exception
 {
+    var makeup = Map.of("gameID", gameID, "playerColor", playerColor); //ID and Color
+    request("PUT", "/game", makeup, authToken, null);
+}
 
+public void clear() throws Exception
+{
+    request("DELETE", "/db", null, null, null); //Clear Database
 }
 
 private <T> T request(String method, String path, Object body, String authToken, Class<T> response) throws Exception
 {
     URL url = (new URI(serverUrl + path)).toURL();
-    HttpURLConnection connection = (HttpURLConnection) + url.openConnection();
+    HttpURLConnection connection = (HttpURLConnection) url.openConnection();
     connection.setRequestMethod(method);
     connection.setDoOutput(true);
 
     if (authToken != null)
     {
-        connection.setRequestMethod("authorization", authToken);
+        connection.setRequestProperty("authorization", authToken); //logout, createGame, listGames, joinGame
     }
 
     if(body != null)
