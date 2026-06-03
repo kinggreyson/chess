ba4dcd61b0a434b94c900ce0a6bb5aab9ce996ca
kinggreyson <kingg14@byu.edu>
2026-06-02 22:48:45 -0600

    BoardDraw functions prepared

--- client/src/main/java/ui/BoardDraw.java
+++ client/src/main/java/ui/BoardDraw.java
@@ -1,4 +1,61 @@
 package ui;
 
+import chess.*;
+import static ui.EscapeSequences.*;
+
 public class BoardDraw {
+
+    //Board Color
+    private static final String lightSquare = SET_BG_COLOR_CREAM;
+    private static final String darkSquare = SET_BG_COLOR_DARK_GREEN;
+    private static final String border = SET_BG_COLOR_DARK_GREY;
+
+    //Piece Colors
+    private static final String white = SET_TEXT_COLOR_WHITE + SET_TEXT_BOLD;
+    private static final String black = SET_TEXT_COLOR_BLACK + SET_TEXT_BOLD;
+
+    //Setup Board
+    public static void board(ChessBoard board, boolean isWhitePerspective)
+    {
+        StringBuilder build = new StringBuilder();
+        build.append(RESET_BG_COLOR).append(RESET_TEXT_COLOR).append("/n");
+
+        if(isWhitePerspective)
+        {
+            whiteBoard(build, board);
+        }
+        else
+        {
+            blackBoard(build, board);
+        }
+        build.append(RESET_BG_COLOR).append(RESET_TEXT_COLOR).append("/n");
+        System.out.print(build);
+    }
+
+    private static void whiteBoard(StringBuilder build, ChessBoard board) //perspective from white
+    {
+        columnLabel(build, true);
+        for (int row = 8; row >= 1; row--)
+        {
+            rowBuild(build, board, row, false);
+        }
+    }
+
+    private static void blackBoard(StringBuilder build, ChessBoard board) //perspective from black
+    {
+
+    }
+
+    private static void columnLabel(StringBuilder build, boolean white)
+    {
+
+    }
+
+    private static void rowBuild(StringBuilder build, ChessBoard board, int row, boolean white)
+    {
+
+    }
+
+
+
 }
--- client/src/main/java/ui/EscapeSequences.java
+++ client/src/main/java/ui/EscapeSequences.java
@@ -45,6 +45,9 @@ public class EscapeSequences {
     public static final String SET_BG_COLOR_BLUE = SET_BG_COLOR + "12m";
     public static final String SET_BG_COLOR_MAGENTA = SET_BG_COLOR + "5m";
     public static final String SET_BG_COLOR_WHITE = SET_BG_COLOR + "15m";
+
+    public static final String SET_BG_COLOR_CREAM = SET_BG_COLOR + "230m"; //ADDED CUSTOM CREAM COLOR FOR LIGHT TILES
+
     public static final String RESET_BG_COLOR = UNICODE_ESCAPE + "[49m";
 
     public static final String WHITE_KING = " ♔ ";
