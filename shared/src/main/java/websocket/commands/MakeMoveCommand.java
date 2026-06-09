ebab6a4b340365633c38ecd87fdc2a2c136da899
kinggreyson <kingg14@byu.edu>
2026-06-08 21:11:59 -0600

    Phase 6 MakeMoveCommand subclass implemented

--- shared/src/main/java/websocket/commands/MakeMoveCommand.java
+++ shared/src/main/java/websocket/commands/MakeMoveCommand.java
@@ -1,4 +1,19 @@
 package websocket.commands;
 
-public class MakeMoveCommand {
+import chess.ChessMove;
+
+public class MakeMoveCommand extends UserGameCommand {
+    private final ChessMove move;
+
+    public MakeMoveCommand(String authToken, int gameID, ChessMove move)
+    {
+        super(CommandType.MAKE_MOVE, authToken, gameID); //UserGameCommand(Parent class handles)
+        this.move = move;
+    }
+
+    public ChessMove moveResult()
+    {
+        return move;
+    }
+
 }
